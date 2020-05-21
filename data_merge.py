d98d6ba8c361389e184873b681aa9afe0bb783b7
Ali Pelczar <apelczar@uchicago.edu>
2020-05-20 21:35:39 -0500

    Fixed denominators in some ACS vars

--- data_merge.py
+++ data_merge.py
@@ -28,12 +28,17 @@ B03001_002E - non hispanic pop
 B03001_003E - hispanic pop
 
 B28002_013E - no internet
+B28011_001E - internet total
 
+B15003_001E - education total
 B15003_022E - bachelor's degree
-B23006_023E - bachelor's degree or higher
+B15003_023E - master's degree
+B15003_024E - professional degree
+B15003_025E - doctorate degree
 
 B19013_001E - median family income
 
+C24050_001E - industry by occupation total
 C24050_002E - agriculture, forestry, fishing and hunting, mining
 C24050_003E - construction
 C24050_004E - manufacturing
@@ -130,8 +135,9 @@ dataset = "acs/acs5"
 base_url = "/".join([HOST, year, dataset])
 predicates = {}
 get_vars = ['NAME', 'B02001_001E', 'B02001_002E', 'B02001_003E', 'B03001_003E',
-            'B28002_013E', 'B15003_022E', 'B23006_023E', 'B19013_001E',
-            'C24050_012E']
+            'B28002_013E', 'B28011_001E', 'B15003_001E', 'B15003_022E', 
+            'B15003_023E', 'B15003_024E', 'B15003_025E',
+            'B19013_001E', 'C24050_001E', 'C24050_012E']
 predicates["get"] = ",".join(get_vars)
 predicates["for"] = "county:*"
 r = requests.get(base_url, params=predicates)
@@ -145,8 +151,9 @@ df['state_name'] = df['NAME'].str.extract(', (.*)')
 #Clean columns and column names
 #Change column types to int
 cols_to_change = ['B02001_001E', 'B02001_002E', 'B02001_003E', 'B03001_003E',
-            'B28002_013E', 'B15003_022E', 'B23006_023E', 'B19013_001E',
-            'C24050_012E']
+            'B28002_013E', 'B28011_001E', 'B15003_001E', 'B15003_022E', 
+            'B15003_023E', 'B15003_024E', 'B15003_025E',
+            'B19013_001E', 'C24050_001E', 'C24050_012E']
 for col in cols_to_change:
     df[col] = pd.to_numeric(df[col])
 
@@ -156,20 +163,24 @@ df['prop_white'] = df['B02001_002E'] / df['total_pop']
 df['prop_black'] = df['B02001_003E'] / df['total_pop']
 df['prop_hisp'] = df['B03001_003E'] / df['total_pop']
 
+#Income
 df['log_med_income'] = np.log(df['median_income'])
 
 #Internet access
-df['prop_no_internet'] = df['B28002_013E'] / df['total_pop']
+df['prop_no_internet'] = df['B28002_013E'] / df['B28011_001E']
 
 #Education
-df['prop_ba'] = df['B15003_022E'] / df['total_pop']
+df['prop_ba'] = (df['B15003_022E'] + df['B15003_023E'] + df['B15003_024E'] +
+                 df['B15003_025E']) / df['B15003_001E']
 
 #Occupation
-df['prop_services'] = df['C24050_012E'] / df['total_pop']
+df['prop_services'] = df['C24050_012E'] / df['C24050_001E']
 
 #Drop extra columns
 df_clean = df.drop(['B02001_002E', 'B02001_003E', 'B03001_003E',
-            'B28002_013E', 'B15003_022E', 'B23006_023E', 'C24050_012E'], axis=1)
+            'B28002_013E', 'B28011_001E', 'B15003_022E', 'B15003_023E', 
+            'B15003_024E', 'B15003_025E', 'B15003_001E', 'C24050_001E',
+            'C24050_012E'], axis=1)
 
 
 #Get population density
@@ -206,6 +217,7 @@ merged = merged.merge(jhu_testing, how='inner', on='state_name')
 govparty = pd.read_csv('data/kff_statepoliticalparties.csv', skiprows=2)
 govparty = govparty.loc[1:, ['Location', 'Governor Political Affiliation']]
 govparty.rename({'Location': 'state_name', 'Governor Political Affiliation': 'gov_party'}, axis=1, inplace=True)
+
 # Convert gov_party to a boolean
 govparty['gov_party'] = govparty['gov_party'] = [
     1 if (x == 'Republican') else 0 for x in govparty['gov_party']]
